32d54952affac20dff3f16ad141d38a834995c74
Jonathan Porta <jonathan@jonathanPorta.com>
2016-06-29 19:57:03 -0600

    Fix CI

--- test/integration/installer/serverspec/install_sh_spec.rb
+++ test/integration/installer/serverspec/install_sh_spec.rb
@@ -14,14 +14,19 @@ describe 'mtna_server_test::install.sh' do
     it { should be_running }
   end
 
-  describe service('autochef.service') do
-    it { should be_installed }
-  end
-
   describe file('/opt/PronghornDigital/mtna-server-cookbook/autochef.sh') do
     it { should be_file }
     it { should be_mode 775 }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'mtna' }
   end
+
+  # TODO: be_installed is not implemented for service types yet. Workaround:
+  describe file('/etc/systemd/system/autochef.service') do
+    it { should be_file }
+  end
+
+  # describe service('autochef.service') do
+  #   it { should be_installed }
+  # end
 end
